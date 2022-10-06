240fdb35b6da32d7da58958ace72bb13b3f28aad
AhmadSyafarudin <ahmadsyafar123@gmail.com>
2022-10-06 13:39:28 +0700

    View ToDo List

--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -1,41 +1,41 @@
 import React, { useState } from 'react';
 
 function Form(props) {
-const [title, setTitle] = useState('');
-const [desc, setDesc] = useState('');
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
 
-const changeTitle = (event) => {
-	setTitle(event.target.value);
-};
+    const changeTitle = (event) => {
+        setTitle(event.target.value);
+    };
 
-const changeDesc = (event) => {
-	setDesc(event.target.value);
-};
+    const changeDesc = (event) => {
+        setDesc(event.target.value);
+    };
 
-const transferValue = (event) => {
-	event.preventDefault();
-	const val = {
-	    title,
-	    desc,
-	};
-	props.func(val);
-	clearState();
-};
+    const transferValue = (event) => {
+        event.preventDefault();
+        const val = {
+            title,
+            desc,
+        };
+        props.func(val);
+        clearState();
+    };
 
-const clearState = () => {
-	setTitle('');
-	setDesc('');
-};
+    const clearState = () => {
+        setTitle('');
+        setDesc('');
+    };
 
-return (
-	<div className="py-3 px-4">
-	    <label className="font-sans text-base font-medium text-white px-2">ToDo </label>
-	    <input className="mr-4" type="text" value={title} onChange={changeTitle} />
-	    <label className="font-sans text-base font-medium text-white px-2">Keterangan</label>
-	    <input className="mr-4" type="text" value={desc} onChange={changeDesc} />
-	    <button  className="font-sans text-base font-medium text-white px-2 bg-blue-500" onClick={transferValue}>Tambah</button>
-	</div>
-);
+    return (
+        <div className="py-3 px-4">
+            <label className="font-sans text-base font-medium text-white px-2">ToDo </label>
+            <input className="mr-4" type="text" value={title} onChange={changeTitle} />
+            <label className="font-sans text-base font-medium text-white px-2">Keterangan</label>
+            <input className="mr-4" type="text" value={desc} onChange={changeDesc} />
+            <button  className="font-sans text-base font-medium text-white px-2 bg-blue-500" onClick={transferValue}>Tambah</button>
+        </div>
+    );
 }
 
 export default Form;
